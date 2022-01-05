b961bfc82a455c6aa3fcfd5dcb653beef0849817
BhargavMantha <manthabhargav@gmail.com>
2022-01-05 20:34:44 +0530

    Added Search Component

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 export class SearchComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+  }
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onFormSubmit(this.state.term);
+  };
   render() {
-    return <div>Search Component</div>;
+    return (
+      <div className="search-bar ui container segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              onChange={(e) => this.setState({ term: e.target.value })}
+              value={this.state.term}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
